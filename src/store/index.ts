84c0e8726fdaec5d7e3fa5d1b2051df551e41df3
hitalojacome <jacomehitalo@gmail.com>
2024-06-14 14:20:39 -0300

    adicionando crud

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,9 @@ import IProjeto from "@/interfaces/IProjeto"
 import INotificacao from "@/interfaces/INotificacao"
 import { InjectionKey } from "vue"
 import { Store, createStore, useStore as vuexUseStore } from "vuex"
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes"
+import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes"
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO } from "./tipo-acoes"
+import http from "@/http"
 
 interface Estado {
     projetos: IProjeto[],
@@ -32,6 +34,9 @@ export const store = createStore<Estado>({
         [EXCLUIR_PROJETO] (state, id: string) {
             state.projetos = state.projetos.filter(proj => proj.id != id)
         },
+        [DEFINIR_PROJETOS] (state, projetos: IProjeto[]) {
+            state.projetos = projetos
+        },
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
             novaNotificacao.id = new Date().getTime()
             state.notificacoes.push(novaNotificacao)
@@ -40,7 +45,25 @@ export const store = createStore<Estado>({
                 state.notificacoes = state.notificacoes.filter(n => n.id != novaNotificacao.id)
             }, 2000)
         }
-    }
+    },
+    actions: {
+        [OBTER_PROJETOS] ({ commit }) {
+            http.get('projetos')
+                .then(resposta => commit (DEFINIR_PROJETOS, resposta.data))
+        },
+        [CADASTRAR_PROJETO] (contexto, nomeDoProjeto: string) {
+            return http.post('/projetos', {
+                nome: nomeDoProjeto
+            })
+        },
+        [ALTERAR_PROJETO] (contexto, projeto: IProjeto) {
+            return http.put(`/projetos/${projeto.id}`, projeto)
+        },
+        [REMOVER_PROJETO] ({ commit }, id: string) {
+            return http.delete(`/projetos/${id}`)
+                .then(() => commit(EXCLUIR_PROJETO, id))
+        }
+    } 
 })
 
 export function useStore(): Store<Estado> {
